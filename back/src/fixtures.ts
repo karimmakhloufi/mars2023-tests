2cd0d394f6df0e4ad8ab9def90d201696e445532
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2023-08-31 11:14:28 +0200

    removed fixtures to make integration test work again

--- back/src/fixtures.ts
+++ back/src/fixtures.ts
@@ -3,10 +3,10 @@ import { Wilder } from "./entity/wilder";
 
 const start = async (): Promise<void> => {
   await dataSource.initialize();
-  await dataSource.dropDatabase();
-  await dataSource.synchronize();
-  await dataSource.getRepository(Wilder).save({ name: "test" });
-  console.log(await dataSource.getRepository(Wilder).find());
+  // await dataSource.dropDatabase();
+  // await dataSource.synchronize();
+  // await dataSource.getRepository(Wilder).save({ name: "test" });
+  // console.log(await dataSource.getRepository(Wilder).find());
 };
 
 void start();
