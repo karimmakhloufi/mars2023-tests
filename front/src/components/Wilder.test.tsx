86a4de8b840621afe2306a3a3758338105268def
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2023-08-31 10:55:35 +0200

    Failing test to show that rule works

--- front/src/components/Wilder.test.tsx
+++ front/src/components/Wilder.test.tsx
@@ -5,7 +5,7 @@ import Wilder from "./Wilder";
 test("renders Wilder with name, city and hardcoded text", () => {
   render(<Wilder name={"Alain"} city={"Paris"} skills={[]} />);
   expect(screen.getByText(/Alain/i)).toBeInTheDocument();
-  expect(screen.getByText(/Paris/i)).toBeInTheDocument();
+  expect(screen.getByText(/Reims/i)).toBeInTheDocument();
   expect(screen.getByText(/Lorem Ipsum/i)).toBeInTheDocument();
   expect(screen.getByText(/Wild Skills/i)).toBeInTheDocument();
 });
